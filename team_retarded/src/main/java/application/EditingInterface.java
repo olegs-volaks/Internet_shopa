16ae6ce3db13ce34bc58f162fd2e43603b8eb88c
Olegs Volaks <olegs.volaks@gmail.com>
2020-11-03 22:08:17 +0200

    fix project structure

--- team_retarded/src/main/java/application/EditingInterface.java
+++ team_retarded/src/main/java/application/EditingInterface.java
@@ -1,4 +1,4 @@
-package main.java.application;
+package application;
 
 public interface EditingInterface {
 
--- team_retarded/src/main/java/application/OperationInterface.java
+++ team_retarded/src/main/java/application/OperationInterface.java
@@ -1,4 +1,4 @@
-package main.java.application;
+package application;
 
 import java.util.List;
 
