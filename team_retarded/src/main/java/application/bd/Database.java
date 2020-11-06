360a696e9b5db872fc2de12874948e9490a479e6
Olegs Volaks <olegs.volaks@gmail.com>
2020-11-07 01:41:21 +0200

    Change database

---
+++ team_retarded/src/main/java/application/bd/Database.java
@@ -0,0 +1,25 @@
+package application.bd;
+
+import application.items.Product;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public interface Database {
+
+    void add(String productName, String specification, double price);
+
+    void delete(long id);
+
+    void delete(Predicate<Product> predicate);
+
+    void clear();
+
+    Optional<Product> getById(long id);
+
+    List<Product> filter(Predicate<Product> predicate);
+
+    List<Product> getList();
+
+}
---
+++ team_retarded/src/main/java/application/bd/ProductListDatabase.java
@@ -0,0 +1,56 @@
+package application.bd;
+
+import application.items.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.toList;
+
+public class ProductListDatabase implements Database {
+
+    private final List<Product> db = new ArrayList<>();
+    private long id = 1;
+
+    @Override
+    public void add(String name, String description, double price) {
+        db.add(new Product(id, name, description, price));
+        id++;
+    }
+
+    @Override
+    public void delete(long id) {
+        db.removeIf(x -> x.getId() == id);
+    }
+
+    @Override
+    public void delete(Predicate<Product> predicate) {
+        db.removeIf(predicate);
+    }
+
+    @Override
+    public void clear() {
+        db.clear();
+    }
+
+    @Override
+    public Optional<Product> getById(long id) {
+        return db.stream()
+                .filter(t -> t.getId() == id)
+                .findAny();
+    }
+
+    @Override
+    public List<Product> filter(Predicate<Product> predicate) {
+        return db.stream()
+                .filter(predicate)
+                .collect(toList());
+    }
+
+    @Override
+    public List<Product> getList() {
+        return db;
+    }
+}
