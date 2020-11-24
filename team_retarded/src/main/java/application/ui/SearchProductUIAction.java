f69faef535072ce0b4961789782db77e266613d2
Dmitrijs <67970833+dgrigorjevs@users.noreply.github.com>
2020-11-24 13:24:21 +0200

    done with search product/ waiting for review

--- team_retarded/src/main/java/application/ui/SearchProductUIAction.java
+++ team_retarded/src/main/java/application/ui/SearchProductUIAction.java
@@ -1,6 +1,10 @@
 package application.ui;
 
+import application.core.requests.SearchProductRequest;
+import application.core.responses.SearchProductResponse;
 import application.core.services.SearchProductService;
+
+import java.util.Scanner;
 
 public class SearchProductUIAction implements UIAction {
 
@@ -12,6 +16,17 @@ public class SearchProductUIAction implements UIAction {
 
     @Override
     public void execute() {
-        //todo: добавить логику
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter a product name : ");
+        String name = scanner.nextLine();
+        System.out.println("Please, enter a product description: ");
+        String description = scanner.nextLine();
+        SearchProductResponse response = service.execute(new SearchProductRequest(name,description));
+        if (response.hasErrors()) {
+            response.getErrors().forEach(coreError -> System.out.println("Error in the field -  "
+            + coreError.getField() + ": " + coreError.getMessage()));
+        } else {
+            System.out.println("Product " + response.getProducts() + " was successfully find") ;
+        }
     }
 }
