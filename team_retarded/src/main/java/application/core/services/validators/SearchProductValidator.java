0436112e031f24298b58889486a46856d06b3ff8
Oļegs Voļaks <67970878+olegs-volaks@users.noreply.github.com>
2020-11-25 15:35:28 +0200

    Download SearchProductValidator.java from DEV

--- team_retarded/src/main/java/application/core/services/validators/SearchProductValidator.java
+++ team_retarded/src/main/java/application/core/services/validators/SearchProductValidator.java
@@ -5,21 +5,12 @@ import application.core.responses.CoreError;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class SearchProductValidator {
 
     public List<CoreError> validate(SearchProductRequest request) {
         List<CoreError> errors = new ArrayList<>();
-        validateNameAndDescription(request).ifPresent(errors::add);
+        //todo: Добавить логику валидации
         return errors;
-    }
-
-    private Optional<CoreError> validateNameAndDescription(SearchProductRequest request) {
-        if ((request.getName() == null || request.getName().isEmpty())&&
-        (request.getDescription() == null || request.getDescription().isEmpty())) {
-            return Optional.of(new CoreError("Name,Description","Both must be not empty!"));
-        }
-        return Optional.empty();
     }
 }
