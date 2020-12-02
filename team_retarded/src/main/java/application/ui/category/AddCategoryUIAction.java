9998fc0daa8653e0d117fee3ececb8349290c08d
Olegs Volaks <olegs.volaks@gmail.com>
2020-12-02 15:26:40 +0200

    Add AddCategory

--- team_retarded/src/main/java/application/ui/category/AddCategoryUIAction.java
+++ team_retarded/src/main/java/application/ui/category/AddCategoryUIAction.java
@@ -1,7 +1,11 @@
 package application.ui.category;
 
+import application.core.requests.category.AddCategoryRequest;
+import application.core.responses.category.AddCategoryResponse;
 import application.core.services.category.AddCategoryService;
 import application.ui.UIAction;
+
+import java.util.Scanner;
 
 public class AddCategoryUIAction implements UIAction {
 
@@ -13,6 +17,15 @@ public class AddCategoryUIAction implements UIAction {
 
     @Override
     public void execute() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, enter new category name: ");
+        String name = scanner.nextLine();
+        AddCategoryResponse response = service.execute(new AddCategoryRequest(name));
+        if (response.hasErrors()) {
+            response.getErrors().forEach(coreError -> System.out.println("Error in the field - "
+                    + coreError.getField() + ": " + coreError.getMessage()));
+        } else {
+            System.out.println("Category Nr " + response.getCategoryId() + " was added successfully");
+        }
     }
 }
