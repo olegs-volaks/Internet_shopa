7201ad14dc6db3fbd6b59f665a064f901d8cfb7f
saturn7888 <saturn@lmr.lv>
2020-12-02 18:04:41 +0200

    тест который меня победил

--- team_retarded/src/test/java/application/core/services/product/SearchProductServiceTest.java
+++ team_retarded/src/test/java/application/core/services/product/SearchProductServiceTest.java
@@ -4,40 +4,29 @@ import application.core.requests.product.SearchProductRequest;
 import application.core.responses.CoreError;
 import application.core.responses.product.SearchProductResponse;
 import application.core.services.validators.product.SearchProductValidator;
-import application.database.ListProductDatabase;
 import application.database.ProductDatabase;
-import application.database.categories.database.CategoriesDatabase;
-import application.database.categories.database.ListCategoriesDatabase;
+import application.items.Product;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
-//@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 class SearchProductServiceTest {
-    //private SearchProductValidator validator;
-    //private  SearchProductService service;
-   // @Before
-            //public void setup(){
-        //validator= Mockito.mock(SearchProductValidator.class);
-        //service =new SearchProductService(null, validator);
-    //}
-
-
-   // @Mock
-    //private SearchProductValidator validator ;
-    //@Mock
-    //private ProductDatabase dbActual;
-    //@InjectMocks
-    ///private SearchProductService service;
-
-    CategoriesDatabase categoriesDatabase = new ListCategoriesDatabase();
-    ProductDatabase dbActual = new ListProductDatabase( categoriesDatabase);
-    SearchProductValidator validator = new SearchProductValidator();
-    SearchProductService service= new SearchProductService(dbActual,validator);
+    @Mock
+    private SearchProductValidator validator ;
+    @Mock
+    private ProductDatabase db;
+    @InjectMocks
+    private SearchProductService service;
 
     @Test
     public void shouldReturnResponseWithErrorsWhenValidatorFails() {
@@ -46,17 +35,139 @@ class SearchProductServiceTest {
         List<CoreError> errors = new ArrayList<>();
         errors.add(new CoreError("name",        "Must not be empty!"));
         errors.add(new CoreError("description", "Must not be empty!"));
-        //Mockito.when((validator.validate(request))).thenReturn(errors);
+        Mockito.when((validator.validate(request))).thenReturn(errors);
 
         SearchProductResponse response = service.execute(request);
         assertTrue(response.hasErrors());
         assertEquals(response.getErrors().size(), 2);
         assertEquals(response.getErrors().get(0).getField(),"name");
 
-        //Mockito.verify(validator).validate(request);
-        //Mockito.verify(validator).validate(any());
-        //Mockito.verifyNoInteractions(dbActual);
+        Mockito.verify(validator).validate(request);
+        Mockito.verify(validator).validate(any());
+        Mockito.verifyNoInteractions(db);
     }
+
+    @Test
+    public void shouldSearchByTitle() {
+        SearchProductRequest request = new SearchProductRequest("Title", null);
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Title", "Author123456789", 345));
+
+        Mockito.when(db.filter(any())).thenReturn(products);
+
+        SearchProductResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertEquals(response.getProducts().size(), 1);
+        assertEquals(response.getProducts().get(0).getName(), "Title");
+        assertEquals(response.getProducts().get(0).getDescription(), "Author123456789");
+    }
+
+    /*@Test
+    public void shouldSearchByAuthor() {
+        SearchProductRequest request = new SearchProductRequest(null, "Author");
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Title", "Author123456789", 345));
+        Mockito.when(db.filter(product -> product.getDescription().contains("Author"))).thenReturn(products);
+
+        SearchProductResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertEquals(response.getProducts().size(), 1);
+        assertEquals(response.getProducts().get(0).getName(), "Title");
+        assertEquals(response.getProducts().get(0).getDescription(), "Author123456789");
+    }
+
+    /*@Test
+    public void shouldSearchByTitleAndAuthor() {
+        SearchBooksRequest request = new SearchBooksRequest("Title", "Author");
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Title", "Author"));
+        Mockito.when(database.findByTitleAndAuthor("Title", "Author")).thenReturn(books);
+
+        SearchBooksResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertEquals(response.getBooks().size(), 1);
+        assertEquals(response.getBooks().get(0).getTitle(), "Title");
+        assertEquals(response.getBooks().get(0).getAuthor(), "Author");
+    }
+
+    @Test
+    public void shouldSearchByTitleWithOrderingAscending() {
+        Ordering ordering = new Ordering("author", "ASCENDING");
+        SearchBooksRequest request = new SearchBooksRequest("Title", null, ordering);
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Title", "Author2"));
+        books.add(new Book("Title", "Author1"));
+        Mockito.when(database.findByTitle("Title")).thenReturn(books);
+
+        SearchBooksResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertEquals(response.getBooks().size(), 2);
+        assertEquals(response.getBooks().get(0).getAuthor(), "Author1");
+        assertEquals(response.getBooks().get(1).getAuthor(), "Author2");
+    }
+
+    @Test
+    public void shouldSearchByTitleWithOrderingDescending() {
+        Ordering ordering = new Ordering("author", "DESCENDING");
+        SearchBooksRequest request = new SearchBooksRequest("Title", null, ordering);
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Title", "Author1"));
+        books.add(new Book("Title", "Author2"));
+        Mockito.when(database.findByTitle("Title")).thenReturn(books);
+
+        SearchBooksResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertEquals(response.getBooks().size(), 2);
+        assertEquals(response.getBooks().get(0).getAuthor(), "Author2");
+        assertEquals(response.getBooks().get(1).getAuthor(), "Author1");
+    }
+
+    @Test
+    public void shouldSearchByTitleWithPagingFirstPage() {
+        Paging paging = new Paging(1, 1);
+        SearchBooksRequest request = new SearchBooksRequest("Title", null, null, paging);
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Title", "Author1"));
+        books.add(new Book("Title", "Author2"));
+        Mockito.when(database.findByTitle("Title")).thenReturn(books);
+
+        SearchBooksResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertEquals(response.getBooks().size(), 1);
+        assertEquals(response.getBooks().get(0).getTitle(), "Title");
+        assertEquals(response.getBooks().get(0).getAuthor(), "Author1");
+    }
+
+    @Test
+    public void shouldSearchByTitleWithPagingSecondPage() {
+        Paging paging = new Paging(2, 1);
+        SearchBooksRequest request = new SearchBooksRequest("Title", null, null, paging);
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Title", "Author1"));
+        books.add(new Book("Title", "Author2"));
+        Mockito.when(database.findByTitle("Title")).thenReturn(books);
+
+        SearchBooksResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertEquals(response.getBooks().size(), 1);
+        assertEquals(response.getBooks().get(0).getTitle(), "Title");
+        assertEquals(response.getBooks().get(0).getAuthor(), "Author2");
+    }*/
+
 
 
 
