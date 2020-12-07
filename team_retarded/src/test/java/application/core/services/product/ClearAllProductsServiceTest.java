682f524ac5abaeb8c1f0a04e8ba326bd26617dfe
Dmitrijs <67970833+dgrigorjevs@users.noreply.github.com>
2020-12-07 22:09:37 +0200

    need to check

--- team_retarded/src/test/java/application/core/services/product/ClearAllProductsServiceTest.java
+++ team_retarded/src/test/java/application/core/services/product/ClearAllProductsServiceTest.java
@@ -1,31 +1,33 @@
 package application.core.services.product;
 
-import application.core.requests.product.ClearAllProductsRequest;
-import application.core.responses.CoreError;
 import application.core.responses.product.ClearAllProductsResponse;
-import application.core.services.validators.product.DeleteProductValidator;
 import application.database.ProductDatabase;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @ExtendWith(MockitoExtension.class)
 public class ClearAllProductsServiceTest {
 
     @Mock
-    private ProductDatabase db;
+    private ProductDatabase database;
 
     @InjectMocks
     private ClearAllProductsService subject;
 
     @Test
-    public void shouldDeleteBookWithIdFromDatabase() {
+    public void should_clear_all_products() {
+        ClearAllProductsResponse response = subject.execute();
+        assertThat(response.hasErrors()).isFalse();
+        Mockito.verify(database).clear();
 
 
     }
