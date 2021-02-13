ab34af443aebffe6580953192c7197e1a5f099ae
Olegs Volaks <olegs.volaks@gmail.com>
2021-02-13 19:40:33 +0200

    Added JpaRepositories

---
+++ team_retarded/src/main/java/eu/retarded/internetstore/database/CartRepository.java
@@ -0,0 +1,8 @@
+package eu.retarded.internetstore.database;
+
+import eu.retarded.internetstore.core.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+}
