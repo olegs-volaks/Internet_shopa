5cc14030098b4695be706a51bbfd2ff93da30d8e
Olegs Volaks <olegs.volaks@gmail.com>
2021-02-16 17:06:05 +0200

    Added csrf security

--- team_retarded/src/main/java/eu/retarded/internetstore/config/WebSecurityConfiguration.java
+++ team_retarded/src/main/java/eu/retarded/internetstore/config/WebSecurityConfiguration.java
@@ -22,7 +22,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/**").permitAll()
                 .and()
                 .formLogin().loginPage("/").permitAll()
