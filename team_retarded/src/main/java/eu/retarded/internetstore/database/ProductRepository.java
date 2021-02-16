7351682ca15467c07020f0876c40fdf576ade899
Olegs Volaks <olegs.volaks@gmail.com>
2021-02-16 17:18:59 +0200

    Removed unnecessary annotations

--- team_retarded/src/main/java/eu/retarded/internetstore/database/ProductRepository.java
+++ team_retarded/src/main/java/eu/retarded/internetstore/database/ProductRepository.java
@@ -2,11 +2,9 @@ package eu.retarded.internetstore.database;
 
 import eu.retarded.internetstore.core.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
