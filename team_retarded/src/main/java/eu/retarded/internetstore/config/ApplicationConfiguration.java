c86e4ee16b81ddfa44329cd620284f8b3da51792
Olegs Volaks <olegs.volaks@gmail.com>
2021-02-16 17:21:28 +0200

    Removed unnecessary code from ApplicationConfiguration.java

--- team_retarded/src/main/java/eu/retarded/internetstore/config/ApplicationConfiguration.java
+++ team_retarded/src/main/java/eu/retarded/internetstore/config/ApplicationConfiguration.java
@@ -1,6 +1,5 @@
 package eu.retarded.internetstore.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.Properties;
 
 
 @Configuration
@@ -23,95 +20,5 @@ public class ApplicationConfiguration {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public Properties hibernateProperties(
-            @Value("${spring.jpa.show-sql}") Boolean showSql,
-            @Value("${spring.jpa.hibernate.ddl-auto}") String hbm2ddl,
-            @Value("${spring.jpa.properties.hibernate.dialect}") String dialect) {
-        Properties properties = new Properties();
-        properties.put("hibernate.show_sql", showSql);
-        properties.put("hibernate.hbm2ddl.auto", hbm2ddl);
-        properties.put("hibernate.dialect", dialect);
-        return properties;
-    }
-
-//    @Bean
-//    public SessionFactory sessionFactory(@Qualifier("dataSource") DataSource dataSource,
-//                                         @Value("${hibernate.packagesToScan}") String packagesToScan,
-//                                         Properties hibernateProperties
-//    ) throws IOException {
-//        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-//        sessionFactoryBean.setPackagesToScan(packagesToScan);
-//        sessionFactoryBean.setDataSource(dataSource);
-//        sessionFactoryBean.setHibernateProperties(hibernateProperties);
-//        sessionFactoryBean.afterPropertiesSet();
-//        return sessionFactoryBean.getObject();
-//    }
-//
-//    @Bean
-//    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
-//        return new HibernateTransactionManager(sessionFactory);
-//    }
-
-    /*
-    @Value("${jdbc.url}")
-    private String jdbcUrl;
-
-    @Value("${driverClass}")
-    private String driverClass;
-
-    @Value("${database.user.name}")
-    private String userName;
-
-    @Value("${database.user.password}")
-    private String password;
-
-
-    @Bean
-    public BasicDataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl(jdbcUrl);
-        dataSource.setDriverClassName(driverClass);
-        dataSource.setUsername(userName);
-        dataSource.setPassword(password);
-        return dataSource;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
-    }
-
-    @Bean
-    public Properties hibernateProperties(
-            @Value("${hibernate.show_sql}") Boolean showSql,
-            @Value("${hibernate.hbm2ddl.auto}") String hbm2ddl,
-            @Value("${hibernate.dialect}") String dialect) {
-        Properties properties = new Properties();
-        properties.put("hibernate.show_sql", showSql);
-        properties.put("hibernate.hbm2ddl.auto", hbm2ddl);
-        properties.put("hibernate.dialect", dialect);
-        return properties;
-    }
-
-    @Bean
-    public SessionFactory sessionFactory(DataSource dataSource,
-                                         @Value("${hibernate.packagesToScan}") String packagesToScan,
-                                         Properties hibernateProperties
-    ) throws IOException {
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setPackagesToScan(packagesToScan);
-        sessionFactoryBean.setDataSource(dataSource);
-        sessionFactoryBean.setHibernateProperties(hibernateProperties);
-        sessionFactoryBean.afterPropertiesSet();
-        return sessionFactoryBean.getObject();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
-        return new HibernateTransactionManager(sessionFactory);
-    }
-*/
 
 }
