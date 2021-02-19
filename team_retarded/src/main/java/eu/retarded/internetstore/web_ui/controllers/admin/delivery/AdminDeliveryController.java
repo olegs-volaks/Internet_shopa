fa8bb9204006089a003b9766a157be459e4b2d59
Dmitrijs <67970833+dgrigorjevs@users.noreply.github.com>
2021-02-19 20:22:33 +0200

    need to fix

---
+++ team_retarded/src/main/java/eu/retarded/internetstore/web_ui/controllers/admin/delivery/AdminDeliveryController.java
@@ -0,0 +1,56 @@
+package eu.retarded.internetstore.web_ui.controllers.admin.delivery;
+
+import eu.retarded.internetstore.core.domain.Delivery;
+import eu.retarded.internetstore.core.requests.delivery.AddDeliveryRequest;
+import eu.retarded.internetstore.core.requests.delivery.GetDeliveryListRequest;
+import eu.retarded.internetstore.core.services.delivery.AddDeliveryService;
+import eu.retarded.internetstore.core.services.delivery.GetDeliveryListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AdminDeliveryController {
+
+    @Autowired
+    private AddDeliveryService addDeliveryService;
+
+    @Autowired
+    private GetDeliveryListService getDeliveryListService;
+
+    @Value("${admin.page-size}")
+    private int pageSize;
+
+    @GetMapping("/admin/delivery/{page}")
+    public String deliveryPage(@PathVariable String page, ModelMap modelMap) {  // productPage
+        int pageInt = Integer.parseInt(page);
+        Page<Delivery> deliveryPage = getDeliveryListService.execute(new GetDeliveryListRequest(
+                PageRequest.of(pageInt - 1, pageSize))).getDeliveriesPage();
+        modelMap.addAttribute("deliveries", deliveryPage); // products
+        modelMap.addAttribute("total_pages", deliveryPage.getTotalPages());
+        modelMap.addAttribute("current_page", pageInt);
+        return "/admin/delivery/index";
+    }
+
+    @GetMapping("/admin/delivery")
+    public String main(ModelMap modelMap) {
+        return "redirect:/admin/delivery/1";
+    }
+
+    @PostMapping("/admin/delivery/add")
+    public String addDelivery(@RequestParam(value = "title") String title,
+                              @RequestParam(value = "region") String region,
+                              @RequestParam(value = "price") double price ) {
+        AddDeliveryRequest addDeliveryRequest = new AddDeliveryRequest(title,region,price);
+        addDeliveryService.execute(addDeliveryRequest);
+        return "redirect:/admin/delivery/1";
+    }
+}
+
--- team_retarded/src/test/java/eu/retarded/internetstore/integrationtests/OrderIntegrationTest.java
+++ team_retarded/src/test/java/eu/retarded/internetstore/integrationtests/OrderIntegrationTest.java
@@ -1,42 +1,72 @@
 package eu.retarded.internetstore.integrationtests;
 
-public class OrderIntegrationTest {
-//    @Autowired
-//    private ApplicationContext context;
-//    @Autowired
-//    private OrderDatabase orderDatabase;
-//
-//    @BeforeEach
-//    void setUp() {
-//        orderDatabase.clear();
-//    }
+import eu.retarded.internetstore.core.requests.order.AddOrderRequest;
+import eu.retarded.internetstore.core.requests.order.DeleteOrderRequest;
+import eu.retarded.internetstore.core.services.order.AddOrderService;
+import eu.retarded.internetstore.core.services.order.DeleteOrderService;
+import eu.retarded.internetstore.database.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-   /* @Test
-    void add_category_test() {
-        AddOrderService addOrderService = context.getBean(AddOrderService.class);
-        long id1 = addOrderService.execute(new AddOrderRequest()).getOrderId();
-        long id2 = addOrderService.execute(new AddOrderRequest()).getOrderId();
-        long id3 = addOrderService.execute(new AddOrderRequest()).getOrderId();
-        assertThat(id1).isLessThan(id2);
-        assertThat(id2).isLessThan(id3);
-        List<Order> result =orderDatabase.getList();
-        assertThat(result.size()).isEqualTo(3);
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/*@SpringBootTest
+public class OrderIntegrationTest {
+
+    @Autowired
+    private ApplicationContext context;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @BeforeEach
+    void setUp() {
+        orderRepository.deleteAll();
     }
 
+    @Test
+    void show_list() {
+        AddOrderService addOrderService = context.getBean(AddOrderService.class);
+        AddOrderRequest request = new AddOrderRequest("admin", "admin12345", "riga", 1, 1,
+                1, 10);
+        addOrderService.execute(request);
+        assertThat(orderRepository.findAll().size()).isEqualTo(1);
+    }
+    // long id1 = addOrderService.execute(new AddOrderRequest();
+    // long id2 = addOrderService.execute(new AddOrderRequest()).getOrderId();
+    // long id3 = addOrderService.execute(new AddOrderRequest()).getOrderId();
+    //  List<Order> result = orderRepository.findAll();
+    //  assertThat(result.size()).isEqualTo(3);
+
+    /*
     @Test
     void delete_category_by_id_test() {
         AddOrderService addOrderService = context.getBean(AddOrderService.class);
         DeleteOrderService deleteOrderService = context.getBean(DeleteOrderService.class);
-        long id1 = addOrderService.execute(new AddOrderRequest()).getOrderId();
-        long id2 = addOrderService.execute(new AddOrderRequest()).getOrderId();
-        long id3 = addOrderService.execute(new AddOrderRequest()).getOrderId();
+        AddOrderRequest request =  new AddOrderRequest("admin","admin12345","riga",1,1,
+                1,10);
+        addOrderService.execute(request);
+        eu.retarded.internetstore.core.domain.Order id = addOrderService.execute(request).getOrder();
+        deleteOrderService.execute(new DeleteOrderRequest(id.getId()));
+        assertThat(orderRepository.findAll().size()).isEqualTo(1);
+        assertThat(orderRepository.existsById(id.getId())).isTrue();
+
+      //  long id1 = addOrderService.execute(new AddOrderRequest()).getOrderId();
+      //  long id2 = addOrderService.execute(new AddOrderRequest()).getOrderId();
+      //  long id3 = addOrderService.execute(new AddOrderRequest()).getOrderId();
         //boolean firstResult = deleteOrderService.execute(new DeleteOrderRequest(id2)).isDeleted();
        // boolean secondResult = deleteOrderService.execute(new DeleteOrderRequest(id3 + 1)).isDeleted();
         //assertThat(firstResult).isTrue();
         //assertThat(secondResult).isFalse();
-        List<Order> resultList = orderDatabase.getList();
-        assertThat(resultList.size()).isEqualTo(2);
-        assertThat(resultList).noneMatch(order -> order.getId() == id2);
-    }*/
-}
+       // List<Order> resultList = orderDatabase.getList();
+       // assertThat(resultList.size()).isEqualTo(2);
+       // assertThat(resultList).noneMatch(order -> order.getId() == id2);
+    }
+
+     */
+
 
