b89ed137af501a21e114e59f83df5216e59f95e6
saturn7888 <saturn@lmr.lv>
2021-02-24 21:45:01 +0200

    исправление

--- team_retarded/src/main/java/eu/retarded/internetstore/database/OrderRepository.java
+++ team_retarded/src/main/java/eu/retarded/internetstore/database/OrderRepository.java
@@ -14,4 +14,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findOrderByUserIdAndStatusEqualsOrStatusEquals(long id, int status1, int status2);
     Page<Order> findOrderByUserIdAndStatusBetween(long id, int status1, int status2, Pageable pageable);
     List<Order> findOrderByUserIdAndStatusBetween(long id, int status1, int status2);
+
+
 }
